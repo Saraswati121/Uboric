626bfa8d369015017c587c1239069087f0bcb196
Rajendra Nali <rajendranali.dakshana17@gmail.com>
2022-05-07 22:51:07 +0530

    data

---
+++ src/category/footware/data/data.js
@@ -0,0 +1,495 @@
+export const Data = [
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2021/01/DSC_0572_result-400x265.jpg',
+    title: 'ADDA MEN’S CHAPPLE / SLIPPERS IN BLUE COLOUR',
+    price: 650.0,
+    offer: '-50%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2021/02/DSC_0831_result-400x265.jpg',
+    title: 'pure cotton men’s printed shirt ',
+    price: 895,
+    offer: '-44%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2021/02/DSC_0775_result-400x265.jpg',
+    title: 'pure cotton men’s printed shirt ',
+    price: 895,
+    offer: '-44%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2021/02/DSC_0807_result-400x265.jpg',
+    title: 'pure cotton men’s printed shirt ',
+    price: 805,
+    offer: '-40%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2021/01/DSC_0580_result-400x265.jpg',
+    title: 'pure cotton men’s printed shirt ',
+    price: 805,
+    offer: '-40%',
+  },
+
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2019/12/7-12551-400x265.jpg',
+    title: 'pure cotton men’s printed shirt ',
+    price: 545,
+    offer: '-40%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2019/11/11-11116-400x267.png',
+    title: 'pure cotton men’s printed shirt ',
+    price: 805,
+    offer: '-40%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2019/11/11-11116-400x267.png',
+    title: 'pure cotton men’s printed shirt ',
+    price: 8995,
+    offer: '-40%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2019/12/7-12551-400x265.jpg',
+    title: 'pure cotton men’s printed shirt ',
+    price: 1005,
+    offer: '-40%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2019/12/7-12551-400x265.jpg',
+    title: 'pure cotton men’s printed shirt ',
+    price: 1805,
+    offer: '-40%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2019/11/8-11001-400x267.png',
+    title: 'pure cotton men’s printed shirt ',
+    price: 895,
+    offer: '-40%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2019/11/8-11001-400x267.png',
+    title: 'pure cotton men’s printed shirt ',
+    price: 8695,
+    offer: '-40%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2019/11/8-11001-400x267.png',
+    title: 'pure cotton men’s printed shirt ',
+    price: 844,
+    offer: '-40%',
+  },
+]
+export const Sneaker = [
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2019/12/13-11091-400x267.png',
+    title: 'Rubi cotton With digital print Half ',
+    price: 7951,
+    offer: '-34%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2019/11/DSC_0122_result-4-400x265.jpg',
+    title: 'MEN SHIRT Magic cotton With digital ',
+    price: 705,
+    offer: '-34%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2019/11/mens_shoes_g.461_DSC_0207-400x300.jpg',
+    title: 'Rubi cotton With digital print Half. ',
+    price: 665,
+    offer: '-34%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2019/11/DSC_0122_result-4-400x265.jpg',
+    title: 'Rubi cotton With digital print Half ',
+    price: 1555,
+    offer: '-34%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2019/11/mens_shoes_g.461_DSC_0207-400x300.jpg',
+    title: 'Rubi cotton With digital print Half ',
+    price: 710,
+    offer: '-34%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2019/11/DSC_0122_result-4-400x265.jpg',
+    title: 'Rubi cotton With digital print Half ',
+    price: 555,
+    offer: '-34%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2019/11/DSC_0122_result-4-400x265.jpg',
+    title: 'Rubi cotton With digital print Half ',
+    price: 5125,
+    offer: '-34%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2019/11/mens_shoes_g.461_DSC_0207-400x300.jpg',
+    title: 'Rubi cotton With digital print Half ',
+    price: 3425,
+    offer: '-34%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2019/11/DSC_0122_result-4-400x265.jpg',
+    title: 'Rubi cotton With digital print Half ',
+    price: 705,
+    offer: '-34%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2019/11/mens_shoes_g.461_DSC_0207-400x300.jpg',
+    title: 'Rubi cotton With digital print Half ',
+    price: 800,
+    offer: '-34%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2019/11/DSC_0122_result-4-400x265.jpg',
+    title: 'Rubi cotton With digital print Half ',
+    price: 7099,
+    offer: '-34%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2019/11/mens_shoes_g.461_DSC_0207-400x300.jpg',
+    title: 'Rubi cotton With digital print Half ',
+    price: 952,
+    offer: '-34%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2019/11/DSC_0122_result-4-400x265.jpg',
+    title: 'Rubi cotton With digital print Half ',
+    price: 777,
+    offer: '-34%',
+  },
+]
+export const Tshirts = [
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2022/02/51mRO9X8L._UL1000_-400x400.jpg',
+    title: 'FINACHI Premium Cotton T-Shirts Combo',
+    price: 990,
+    offer: '-24%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2022/02/51mRO9X8L._UL1000_-400x400.jpg',
+    title: 'FINACHI Premium Cotton T-Shirts Combo',
+    price: 990,
+    offer: '-24%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2022/02/51mRO9X8L._UL1000_-400x400.jpg',
+    title: 'FINACHI Premium Cotton T-Shirts Combo',
+    price: 990,
+    offer: '-24%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2022/02/51mRO9X8L._UL1000_-400x400.jpg',
+    title: 'FINACHI Premium Cotton T-Shirts Combo',
+    price: 990,
+    offer: '-24%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2022/02/61-Qz6jP4QL._UL1000_-400x400.jpg',
+    title: 'FINACHI Vest Combo',
+    price: 990,
+    offer: '-14%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2022/02/41tpscFGC8L._UL1000_-400x400.jpg',
+    title: 'FINACHI Premium Cotton T-Shirt (Black)',
+    price: 399,
+    offer: '-24%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2022/02/41xl2-xcHoL._UL1000_-1-400x400.jpg',
+    title: 'FINACHI Premium Cotton T-Shirt (Dark Blue)',
+    price: 399,
+    offer: '-24%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2022/02/51s-EmuOnnL._UL1000_-1-400x400.jpg',
+    title: 'FINACHI Premium Cotton T-Shirt (Peach Color)',
+    price: 399,
+    offer: '-24%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2022/02/519oLttxcZL._UL1000_-3-400x400.jpg',
+    title: 'FINACHI Premium Cotton T-Shirt (Pista)',
+    price: 399,
+    offer: '-54%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2022/02/618gZB8DiKL._UL1500_-400x400.jpg',
+    title: 'FINACHI Ultra Soft T-Shirt black',
+    price: 990,
+    offer: '-54%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2022/02/61nB1ZuhBCL._UL1500_.jpg',
+    title: 'FINACHI Premium Cotton T-Shirts Combo',
+    price: 990,
+    offer: '-24%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2022/02/61IZ6ATVMFL._UL1500_-400x400.jpg',
+    title: 'FINACHI Premium Cotton T-Shirts Combo',
+    price: 990,
+    offer: '-24%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2022/02/61wHli5TQuL._UL1500_-400x400.jpg',
+    title: 'FINACHI Premium Cotton T-Shirts Combo',
+    price: 990,
+    offer: '-24%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2022/02/51mRO9X8L._UL1000_-400x400.jpg',
+    title: 'FINACHI Premium Cotton T-Shirts Combo',
+    price: 990,
+    offer: '-24%',
+  },
+]
+
+export const TB = [
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2022/03/61uFAIVjljL._UL1500_-400x399.jpg',
+    title: 'FINACHI 100% Combed Bio Wash Cotton',
+    price: 799,
+    offer: '-47%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2022/03/61rewJs4pFL._UL1500_-400x399.jpg',
+    title: 'FINACHI 100% Combed Bio Wash Cotton',
+    price: 799,
+    offer: '-47%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2022/02/81OXF5PEo8L._UL1500_-400x400.jpg',
+    title: 'FINACHI Ultra Soft Cotton Brief (Pack)',
+    price: 799,
+    offer: '-47%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2022/02/81BoaL5NdHL._UL1500_-400x400.jpg',
+    title: 'FINACHI Ultra Soft Cotton Brief (Pack)',
+    price: 799,
+    offer: '-47%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2022/03/61mqFWGAmQL._UL1500_-400x399.jpg',
+    title: 'FINACHI 100% Combed Bio Wash Cotton.',
+    price: 499,
+    offer: '-40%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2022/03/61rzcvHQDTL._UL1500_-400x399.jpg',
+    title: 'FINACHI 100% Combed Bio Wash Cotton.',
+    price: 499,
+    offer: '-40%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2022/02/518vKyF4xL._UL1000_-400x400.jpg',
+    title: 'FINACHI Ultra Soft Modal Brief (Pack).',
+    price: 549,
+    offer: '-40%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2022/02/514CuQ8DsaL._UL1000_-400x400.jpg',
+    title: 'FINACHI Ultra Soft Modal Brief (Pack).',
+    price: 549,
+    offer: '-40%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2022/02/51O73PXl2SL._UL1000_-400x400.jpg',
+    title: 'FINACHI Ultra Soft Modal Trunk (Pack...',
+    price: 649,
+    offer: '-50%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2022/02/51D0eXpOuNL._UL1000_-400x400.jpg',
+    title: 'FINACHI Ultra Soft Modal Trunk (Pack...',
+    price: 649,
+    offer: '-50%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2022/02/513eZSzsvbL._UL1000_-400x400.jpg',
+    title: 'FINACHI Ultra Soft Modal Trunk (Pack...',
+    price: 649,
+    offer: '-50%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2022/02/51c77FDveJL._UL1000_-400x400.jpg',
+    title: 'FINACHI Ultra Soft Modal Trunk (Pack...',
+    price: 649,
+    offer: '-50%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2022/02/51Zhp6D5w9L._UL1000_-400x400.jpg',
+    title: 'FINACHI Ultra Soft Modal Trunk (Pack...',
+    price: 649,
+    offer: '-50%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2022/03/61r-h13dJqL._UL1500_-400x399.jpg',
+    title: 'FINACHI 100% Combed Bio Wash Cotton...',
+    price: 649,
+    offer: '-50%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2022/02/51ZALmsvn7L._UL1000_-400x400.jpg',
+    title: 'FINACHI Ultra Soft Modal Trunk (Pack...',
+    price: 449,
+    offer: '-44%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2022/03/61r-h13dJqL._UL1500_-400x399.jpg',
+    title: 'FINACHI Ultra Soft Modal Trunk (Pack...',
+    price: 449,
+    offer: '-44%',
+  },
+  {
+    img:
+      'https://www.uboric.com/wp-content/uploads/2022/03/51rcWWWF7aL._UL1500_-400x399.jpg',
+    title: 'FINACHI Ultra Soft Modal Trunk (Pack...',
+    price: 449,
+    offer: '-44%',
+  },
+]
+export const womens = [
+  {
+    img: 'https://www.uboric.com/wp-content/uploads/2022/03/071-400x562.jpg',
+    title: 'mint green anarkali set ',
+    price: 5599,
+    offer: '-17%',
+  },
+  {
+    img: 'https://www.uboric.com/wp-content/uploads/2022/03/181.jpg',
+    title: '2 piece jacket dress set',
+    price: 2799,
+    offer: '-17%',
+  },
+  {
+    img: 'https://www.uboric.com/wp-content/uploads/2022/03/101.jpg',
+    title: 'aquamarine cotton suit set ',
+    price: 2899,
+    offer: '-17%',
+  },
+  {
+    img: 'https://www.uboric.com/wp-content/uploads/2022/03/456.jpg',
+    title: 'bandhej dhoti set',
+    price: 2899,
+    offer: '-17%',
+  },
+  {
+    img: 'https://www.uboric.com/wp-content/uploads/2022/03/216-400x563.jpg',
+    title: 'berry blue cotton suit set ',
+    price: 2599,
+    offer: '-17%',
+  },
+  {
+    img: 'https://www.uboric.com/wp-content/uploads/2022/03/415.jpg',
+    title: 'black crown sharara set ',
+    price: 3599,
+    offer: '-17%',
+  },
+  {
+    img: 'https://www.uboric.com/wp-content/uploads/2022/03/320-400x587.jpg',
+    title: 'black polka set',
+    price: 1999,
+    offer: '-17%',
+  },
+  {
+    img: 'https://www.uboric.com/wp-content/uploads/2022/03/225-400x580.jpg',
+    title: 'blue anchor cotton suit set',
+    price: 2399,
+    offer: '-17%',
+  },
+  {
+    img: 'https://www.uboric.com/wp-content/uploads/2022/03/053-400x460.jpg',
+    title: 'mint green anarkali set ',
+    price: 2999,
+    offer: '-17%',
+  },
+  {
+    img: 'https://www.uboric.com/wp-content/uploads/2022/03/272-400x583.jpg',
+    title: 'Buff Orange Cotton Suit Set ',
+    price: 5599,
+    offer: '-17%',
+  },
+  {
+    img: 'https://www.uboric.com/wp-content/uploads/2022/03/154.jpg',
+    title: 'mint green anarkali set ',
+    price: 4599,
+    offer: '-17%',
+  },
+  {
+    img: 'https://www.uboric.com/wp-content/uploads/2022/03/451-400x556.jpg',
+    title: 'calico print drape set ',
+    price: 3599,
+    offer: '-17%',
+  },
+  {
+    img: 'https://www.uboric.com/wp-content/uploads/2022/03/058-400x566.jpg',
+    title: 'cherry red suit set',
+    price: 3599,
+    offer: '-17%',
+  },
+  {
+    img: 'https://www.uboric.com/wp-content/uploads/2022/03/316-400x583.jpg',
+    title: 'chevron kurti set ',
+    price: 3599,
+    offer: '-17%',
+  },
+]
